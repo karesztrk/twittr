0d82ce3baa69d2d345f83e8605aff7a47191c8db
Károly Török <torok.karoly.krisztian@gmail.com>
2022-10-14 17:15:52 +0200

    Tweets list

---
+++ src/routes/home/+error.svelte
@@ -0,0 +1,16 @@
+<script context="module" lang="ts">
+  import { page } from '$app/stores';
+</script>
+
+<div class="error">
+	<h1>{$page.status}: {$page?.error?.message}</h1>
+</div>
+
+<style>
+	.error {
+		display: grid;
+		gap: var(--spacing-32);
+		padding: var(--spacing-24) var(--spacing-32);
+		place-items: center;
+	}
+</style>
